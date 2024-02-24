664388d0334b598c15a6d1e8bafb4f029e031855
Iuliana <sendmailtoiuliana@gmail.com>
2024-02-24 18:42:19 +0200

    changed the js file

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    var hero = document.querySelector('.hero');
-    var heroHeight = hero.offsetHeight;
-    var scrollPosition = window.scrollY + (window.innerHeight / 2); // Scroll position at the center of the viewport
-  
-    if (scrollPosition > hero.offsetTop && scrollPosition < (hero.offsetTop + heroHeight)) {
-      navbar.style.backgroundColor = '#120602'; // Change to desired background color
-    } else {
-      navbar.style.backgroundColor = 'transparent'; // Back to transparent when not in the hero section
-    }
-  });
-  
+  var navbar = document.querySelector('.navbar');
+  var hero = document.querySelector('.hero');
+  var heroHeight = hero.offsetHeight;
+  var scrollPosition = window.scrollY + (window.innerHeight / 2)+180; // Scroll position at the center of the viewport
+  var heroTop = hero.offsetTop;
+  var heroBottom = heroTop + heroHeight;
+
+  if (scrollPosition > heroBottom || scrollPosition < heroTop) {
+    navbar.style.backgroundColor = '#120602'; // Transparent background when outside the hero section
+  } else {
+    navbar.style.backgroundColor = 'transparent'; // Change to desired background color when inside the hero section
+  }
+});
