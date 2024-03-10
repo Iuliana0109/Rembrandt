7db868886c6e37fadb8acede54cc7dd35309de40
Iuliana <sendmailtoiuliana@gmail.com>
2024-03-10 20:57:24 +0200

    hero section for mobile

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 // NAVBAR STUFF
-window.addEventListener('scroll', function() {
-  var navbar = document.querySelector('.navbar');
-  var hero = document.querySelector('.hero');
-  var heroHeight = hero.offsetHeight;
-  var scrollPosition = window.scrollY + (window.innerHeight / 2)+180; // Scroll position at the center of the viewport
-  var heroTop = hero.offsetTop;
-  var heroBottom = heroTop + heroHeight;
 
-  if (scrollPosition > heroBottom || scrollPosition < heroTop) {
-    navbar.style.backgroundColor = '#120602'; // Transparent background when outside the hero section
-  } else {
-    navbar.style.backgroundColor = 'transparent'; // Change to desired background color when inside the hero section
-  }
-});
 
 document.addEventListener("DOMContentLoaded", function() {
   var navLinks = document.querySelectorAll('nav a');
@@ -32,6 +19,10 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+});
+
+document.querySelector('.menu-icon').addEventListener('click', function() {
+  document.querySelector('.navbar').classList.toggle('menu-open');
 });
 
 
