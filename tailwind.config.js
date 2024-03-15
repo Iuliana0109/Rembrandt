3dc580ee3337812a9e78c2bcda204263a210fa8b
Iuliana <sendmailtoiuliana@gmail.com>
2024-03-15 10:50:38 +0200

    rewrote hero section with tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    // Paths to your template files...
+    './index.html'
+  ],
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
